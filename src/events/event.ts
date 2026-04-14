e8f91ab1f3f3b1703e8579b150682dfd63c86e64
Nithi Vankineni <nvankineni@umass.edu>
2026-04-14 13:36:43 -0400

    feat: add event model

---
+++ src/events/event.ts
@@ -0,0 +1,20 @@
+export interface Event {
+    id: string;
+    title: string;
+    description: string;
+    startTime: Date;
+    endTime: Date;
+    capacity?: number;
+    status: "draft" | "published" | "cancelled" | "past";
+    organizerId: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface CreateEventInput {
+    title: string;
+    description: string;
+    startTime: Date;
+    endTime: Date;
+    capacity?: number;
+  }
