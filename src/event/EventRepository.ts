af239966cf54d248a19e495541d442053c210002
Nithi Vankineni <nvankineni@umass.edu>
2026-04-14 23:41:31 -0400

    feat: add getEvent, publishEvent,cancelEvent

--- src/event/EventRepository.ts
+++ src/event/EventRepository.ts
@@ -1,8 +1,13 @@
 import type { Result } from "../lib/result";
-import type { IEvent, EventStatus } from "./Event";
+import type { IEvent, EventStatus, EventDetail } from "./Event";
 import type { EventError } from "./errors";
 
 export interface IEventRepository {
   findById(id: string): Promise<Result<IEvent | null, EventError>>;
   updateStatus(id: string, status: EventStatus): Promise<Result<IEvent, EventError>>;
+  //Feature 2
+  getEvent(eventId: string, requesterId: string): Promise<Result<EventDetail, EventError>>
+  //Feature 5
+  publishEvent(eventId: string, requesterId: string): Promise<Result<IEvent, EventError>>;
+  cancelEvent(eventId: string, requesterId: string): Promise<Result<IEvent, EventError>>;
 }
